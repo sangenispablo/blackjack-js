ded9e46f4ea76c5a4f675c38a221ec4eb987e9c9
sangenispablo <sangenispablo@gmail.com>
2022-05-21 09:57:22 -0300

    agrego metodo valorCarta

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,4 +39,20 @@ const pedirCarta = () => {
     return carta;
 }
 
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor) ? (valor === 'A' ? 11 : 10) : valor * 1);
+    // let puntos = 0;
+    // if (isNaN(valor)) {
+    //     puntos = valor === 'A' ? 11 : 10;
+    // } else {
+    //     puntos = valor * 1;
+    // }
+    // return puntos;
+}
+
+// codigo para probar
 crearDeck();
+const carta = pedirCarta();
+console.log(carta);
+console.log({ valor: valorCarta(carta) });
