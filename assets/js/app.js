33a9d58112e5a6fa97663bac827e88154caf1bce
Pablo Sangenis <PASangenis@teco.com.ar>
2022-05-23 10:00:33 -0300

    primera version terminada-falta refactorizar

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,15 +11,22 @@ const tipos = ["C", "D", "H", "S"];
 // cartas que no son numeros
 const especiales = ["A", "J", "Q", "K"];
 let puntosJugador = 0;
+let puntosComputadora = 0;
 
 // Referencias al HTML
 const btnPedir = document.querySelector("#btnPedir");
+const btnDetener = document.querySelector("#btnDetener");
+const btnNuevo = document.querySelector("#btnNuevo");
+
 const puntosJugadorHTML = document.querySelectorAll("small")[0];
 const puntosComputadoraHTML = document.querySelectorAll("small")[1];
-const jugadorCartas = document.querySelector("#jugador-cartas");
+
+const divJugadorCartas = document.querySelector("#jugador-cartas");
+const divComputadoraCartas = document.querySelector("#computadora-cartas");
 
 // Con esta funcion creo una nueva barajas (seria como mezclar y tener el maso listo para dar)
 const crearDeck = () => {
+  let deck = [];
   for (let i = 2; i <= 10; i++) {
     for (const tipo of tipos) {
       deck.push(`${i}${tipo}`);
@@ -48,32 +55,86 @@ const pedirCarta = () => {
 const valorCarta = (carta) => {
   const valor = carta.substring(0, carta.length - 1);
   return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
-  // let puntos = 0;
-  // if (isNaN(valor)) {
-  //     puntos = valor === 'A' ? 11 : 10;
-  // } else {
-  //     puntos = valor * 1;
-  // }
-  // return puntos;
 };
 
-// // codigo para probar
-// const carta = pedirCarta();
-// console.log(carta);
-// console.log({ valor: valorCarta(carta) });
+const borrarCartas = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
+
+// turno computadora
+const turnoComputadora = (puntosMinimos) => {
+  do {
+    const carta = pedirCarta();
+    puntosComputadora += valorCarta(carta);
+    puntosComputadoraHTML.innerText = puntosComputadora;
+    // creo la carta
+    const imgCarta = document.createElement("img");
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add("carta");
+    divComputadoraCartas.append(imgCarta);
+    if (puntosMinimos > 21) {
+      break;
+    }
+  } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+  setTimeout(() => {
+    // aca evaluo la situación de los puntos
+    if (puntosComputadora === puntosMinimos) {
+      alert("Nadie gana :(");
+    } else if (puntosMinimos > 21) {
+      alert("Computadora Gana");
+    } else if (puntosComputadora > 21) {
+      alert("Jugador Gana");
+    } else {
+      alert("Computadora Gana");
+    }
+  }, 500);
+};
 
 // Eventos de las referencias al HTML
 btnPedir.addEventListener("click", () => {
   const carta = pedirCarta();
   puntosJugador += valorCarta(carta);
   puntosJugadorHTML.innerText = puntosJugador;
-  // creo la carta con
-  // <img class="carta" src="assets/cartas/10C.png" alt="carta">
+  // creo la carta
   const imgCarta = document.createElement("img");
   imgCarta.src = `assets/cartas/${carta}.png`;
   imgCarta.classList.add("carta");
-  jugadorCartas.append(imgCarta);
-  console.log(carta, puntosJugador);
+  divJugadorCartas.append(imgCarta);
+  if (puntosJugador > 21) {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+  } else if (puntosJugador == 21) {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+  }
 });
 
-crearDeck();
+btnDetener.addEventListener("click", () => {
+  btnPedir.disabled = true;
+  btnDetener.disabled = true;
+  turnoComputadora(puntosJugador);
+});
+
+btnNuevo.addEventListener("click", () => {
+  // habilito botones
+  btnPedir.disabled = false;
+  btnDetener.disabled = false;
+  // borro cartas
+  borrarCartas(divComputadoraCartas);
+  borrarCartas(divJugadorCartas);
+  // reset puntajes
+  puntosJugador = 0;
+  puntosComputadora = 0;
+  // pongo en cero los puntos
+  puntosJugadorHTML.innerText = puntosJugador;
+  puntosComputadoraHTML.innerText = puntosComputadora;
+  // creo una baraja nueva
+  deck = crearDeck();
+  // borrar todas las cartas
+});
+
+deck = crearDeck();
